2547a975d47408af9c3906d2ff3d05c5e52ef3bf
HiiroMatsuoka822 <hiiro.bran.gin.378@gmail.com>
2025-05-13 10:23:19 -0700

    HW3

--- HW3/app.py
+++ HW3/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/fortune')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("fortune.html")
+    result = None  # Initialize result
+    if request.method == 'POST':
+        user = request.form['user']
+        color = request.form['color']
+        number = request.form['number']
+        result = f"User: {user}, Favorite Color: {color}, Favorite Number: {number}"
+    return render_template("fortune.html", result=result)
+
 if __name__ == '__main__':
     print("Starting Flask app")
     app.run(debug=True)
-
-
